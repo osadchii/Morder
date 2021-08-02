2be2bbcd11e0bf1872247a03a61ec69acf805630
a.osadchii <roflandcom@gmail.com>
2021-08-02 12:13:33 +0300

    fix tests

--- test/sbermegamarket.test-entity.ts
+++ test/sbermegamarket.test-entity.ts
@@ -1,10 +1,12 @@
 import { SberMegaMarketDto } from '../src/sbermegamarket/dto/sbermegamarket.dto';
 
 export const sberMegaMarketDto: SberMegaMarketDto = {
+  orderBefore: 1,
+  shippingDays: 0,
   name: 'Test SberMegaMarget',
   active: true,
   nullifyStocks: false,
   feedGenerationInterval: 1,
   minimalPrice: 100,
-  outletId: 1,
+  outletId: 1
 };
