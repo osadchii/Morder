c9ed41c4d5a02d7adda7086865b872fdb8eb8c9c
a.osadchii <roflandcom@gmail.com>
2021-09-14 08:23:40 +0300

    auth logs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UserModel } from './user.model';
 import { InjectModel } from 'nestjs-typegoose';
@@ -9,12 +9,15 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+
   constructor(
     @InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
     private readonly jwtService: JwtService,
   ) {}
 
   async createUser(dto: UserDto) {
+    this.logger.log(`Creating user: ${dto.login}`);
     const salt = await genSalt(10);
     const newUser = new this.userModel({
       email: dto.login,
@@ -34,10 +37,12 @@ export class AuthService {
   async validateUser(email: string, password: string) {
     const user = await this.findUser(email);
     if (!user) {
+      this.logger.warn(`Wrong email: ${email}`);
       throw new UnauthorizedException(USER_NOT_FOUND_ERROR);
     }
     const isCorrectPassword = await compare(password, user.passwordHash);
     if (!isCorrectPassword) {
+      this.logger.warn(`Wrong password for user with email: ${email}`);
       throw new UnauthorizedException(WRONG_PASSWORD_ERROR);
     }
     return { email: user.email };
@@ -45,6 +50,7 @@ export class AuthService {
 
   async login(email: string) {
     const payload = { email };
+    this.logger.log(`Logged in user with email: ${email}`);
 
     return {
       access_token: await this.jwtService.signAsync(payload),
